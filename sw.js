13be6f36ef5a1dce5d81d8d4e3682cee489c5fe0
zitakikun12 <wirnawirna82@gmail.com>
2025-07-06 21:56:28 +0800

    Upload project

---
+++ sw.js
@@ -0,0 +1,19 @@
+// sw.js
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({type: 'window'}).then(windowClients => {
+      for (const client of windowClients) {
+        if (client.url === '/' && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow('/');
+      }
+    })
+  );
+});
